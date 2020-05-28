85ae1acf48650c1d7855a06c3331af5a042bcbd8
Irina <ikaradimova@gmail.com>
2020-05-28 21:08:16 +0300

    fresh spring maven project

---
+++ src/main/java/com/example/softwaretoolsv2/SoftwareToolsV2Application.java
@@ -0,0 +1,13 @@
+package com.example.softwaretoolsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareToolsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareToolsV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/softwaretoolsv2/SoftwareToolsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softwaretoolsv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareToolsV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
