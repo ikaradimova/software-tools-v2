65e676b53220b230a0ed31a1ce0e6ccadc369b78
Irina <ikaradimova@gmail.com>
2020-05-28 21:29:03 +0300

    adding simple rest logic

---
+++ src/main/java/com/example/softwaretoolsv2/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.softwaretoolsv2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "Welcome to my Software Tools Project";
+    }
+}
